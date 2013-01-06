74417a2e0ecaa98aa4da6b3ce2e6d32f45a05106
Michał Lipek <michal@lipek.net>
2013-01-06 20:52:48 +0100

    New validator: NotEmptyString

---
+++ src/Nautilus/Validator/NotEmptyString.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Nautilus\Validator;
+
+class NotEmptyString extends Validator
+{
+
+    public function __construct($error = null, $value = null)
+    {
+        parent::__construct(
+            $error ? $error : "Enter some text",
+            $value
+        );
+    }
+
+    public function validate()
+    {
+        return $this->getValue() === '';
+    }
+}
---
+++ tests/src/Nautilus/Validator/NotEmptyStringTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Nautilus\Validator\Tests;
+
+use Nautilus\Validator\NotEmptyString;
+
+class NotEmptyStringTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testPositives()
+    {
+        $validator = new NotEmptyString();
+        $validator->setValue('test');
+        $this->assertFalse($validator->validate());
+
+        $validator->setValue('0');
+        $this->assertFalse($validator->validate());
+
+        $validator->setValue(0);
+        $this->assertFalse($validator->validate());
+    }
+
+    public function testFalses()
+    {
+        $validator = new NotEmptyString();
+        $validator->setValue('');
+        $this->assertTrue($validator->validate());
+    }
+}
